04c52580c3409d7f7f5343d0f831c1b601533435
drasko1959 <drasko.kosovic@gmial.com>
2021-11-02 12:46:05 +0100

    init

--- src/com/company/colekcija/Main.java
+++ src/com/company/colekcija/Main.java
@@ -2,8 +2,11 @@ package com.company.colekcija;
 
 public class Main {
 
+
     public static void main(String[] args){
-
-
+      Monitor monitor= new Monitor(458);
+      Tastatura tastatura= new Tastatura("plava");
+      PC pc= new PC(monitor,tastatura,"Assus",454);
+        System.out.println(pc.getMarka_pc());
     }
 }
--- src/com/company/colekcija/Tastatura.java
+++ src/com/company/colekcija/Tastatura.java
@@ -1,4 +1,19 @@
 package com.company.colekcija;
 
 public class Tastatura {
+
+    String boja;
+
+    public String getBoja() {
+        return boja;
+    }
+
+    public void setBoja(String boja) {
+        this.boja = boja;
+    }
+
+    public Tastatura(String boja) {
+        this.boja = boja;
+    }
+
 }
