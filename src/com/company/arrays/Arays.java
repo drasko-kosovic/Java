7623ea758f8f85c2df8a24b60cd9e0012213f3d7
drasko-kosovic <drasko.kosovic@gmail.com>
2021-11-07 18:47:30 +0100

    init

---
+++ src/com/company/arrays/Arays.java
@@ -0,0 +1,21 @@
+package com.company.arrays;
+
+
+public class Arays {
+
+    public static void main(String[] args) {
+        int[] arry1=new int[3];
+        arry1[0]=2;
+        arry1[1]=3;
+        arry1[2]=4;
+        for(int i=0;i<arry1.length;i++){
+            System.out.println(arry1[i]);
+            System.out.println(arry1.length);
+        }
+
+//for(int array1:novi){
+//    System.out.println(novi);
+//}
+
+    }
+}
