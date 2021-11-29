f28cc028072889072c14d21dbd5ceec782a8e373
drasko1959 <drasko.kosovic@gmial.com>
2021-11-29 13:38:14 +0100

    init

--- src/com/company/LAMBDA/predefinaisane_lambda_funkcije/predicate/predicate2/Demo2.java
+++ src/com/company/LAMBDA/predefinaisane_lambda_funkcije/predicate/predicate2/Demo2.java
@@ -44,13 +44,24 @@ public class Demo2 {
     //Primjer 1
     public static void main(String[] args) {
         Zaposleni zaposleni=new Zaposleni("Kosta",15,1500);
+       //Objekat je pr ubacujemo funkciju lambda :e->(e.z_plata>1000&&e.z_staz>10)
         Predicate<Zaposleni>pr=e->(e.z_plata>1000&&e.z_staz>10);
         System.out.println(pr.test(zaposleni));
+
+        //Primjer 2
+
+        ArrayList <Zaposleni> al=new ArrayList<>();
+        al.add(new Zaposleni("Tom",25,2000));
+        al.add(new Zaposleni("Marko",5,800));
+        al.add(new Zaposleni("Veljko",12,500));
+        for(Zaposleni x:al){
+            if(pr.test(x))
+                System.out.println("Ime je  "+x.z_ime+"  Plata je    "+x.z_plata+"    Staz je "+x.z_staz);
+        }
+
     }
 
-    //Primjer 2
 
-    ArrayList <Zaposleni> al=new ArrayList<>();
 
 
 }
