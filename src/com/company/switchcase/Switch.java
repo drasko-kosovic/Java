bea6d7405b172c2cfe35c93e8cbf42c38aaf5e3d
drasko1959 <drasko.kosovic@gmial.com>
2021-12-13 09:29:24 +0100

    init

---
+++ src/com/company/switchcase/Switch.java
@@ -0,0 +1,16 @@
+package com.company.switchcase;
+
+public class Switch {
+    public static void main(String[] args) {
+
+        int day = 12;
+
+        switch (day) {
+            case 2:
+                System.out.println("Utorak");
+                break;
+            default:
+                System.out.println("Niste izabrali dobar dan");
+        }
+    }
+}
